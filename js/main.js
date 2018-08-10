941e9a835e79bd9f9945464b7dbc36a7cc1cd08e
Ogaga Ivhurie <ivhurie01@yahoo.co.uk>
2018-08-10 11:30:25 +0100

    upgraded_08_10

--- js/main.js
+++ js/main.js
@@ -1,58 +1,51 @@
 var access_token = "pk.eyJ1Ijoib2djaWNlcm8iLCJhIjoiY2prazk2bDFlMTloeDN2cGs0OWdnZ3M3dCJ9.TjHJEC7jposIe4SV254wJA"
-var map = L.map('map', {
-    center: [55.310151,-3.4483919999999997],
-    preferCanvas: false,
-    zoomControl: true,
-    zoom: 13
-}).setView([51.496842,-0.16882], 13);
+var map = L.map('map').setView([51.496842,-0.16882], 13);
 
 L.tileLayer(`https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${access_token}`, {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox.streets',
+    id: 'mapbox.light',
     accessToken: `${access_token}`
 }).addTo(map);
-let latlng = display = coordinates = [];
+let latlng = [], display = [], coordinates = [];
 let recordToLoad = 0;
+let maxRecord = 10;
 let signal;
-
 function loadCoord() {
     console.time("totalTime:");
-    d3.csv("data/Coordinates.csv")
-        .on("progress", function(evt) {
-            console.log("Amount loaded: " + evt.loaded)
-        })
-        .get(function(data) {
-            coordinates = data;
-        });
-    
-}
-
-loadCoord();
-
-setTimeout(() => {
-    recordToLoad = 10;
-    coordinates.forEach(element => {
-        while (recordToLoad > 0) {
-            latlng[0] = (parseFloat(element.Latitude));
-            latlng[1] = (parseFloat(element.Longitude));
+    d3.csv("data/Coordinates.csv", (e) => {
+        return{
+            Latitude: parseFloat(e.Latitude),
+            Longitude: +e.Longitude
+        };
+    }, (d) => {
+        while (recordToLoad < maxRecord) {
+            let values = Object.values(d)
+            latlng[0] = parseFloat(values[0]);
+            latlng[1] = parseFloat(values[1]);
+            display.push(values);
             displayCoord(latlng);
-            recordToLoad--
+            recordToLoad++
             break;
         }
+        
     });
-    
-}, 10000);
+    setTimeout(() => {
 
-
-
+    }, 200);
+        
+    setInterval(()=>{
+        display.forEach(e => {
+            displayCoord(e);
+        });
+    }, 1000)
+}
 function heatCode() {
     var code = Math.round(Math.random() * 10) + 1;
-     return code> 8 ? '#ff0000':
-            code> 6 ? '#ff7802':
-            code> 4 ? '#fff200':
-                      '#027025';
+     return code > 7 ? '#ff0000':
+            code > 5 ? '#ffff00':
+            code > 3 ? '#ffff00':
+                       '#008000';
 }
-
 function displayCoord(coordinates) {
     signal = heatCode();
     L.circle(coordinates, {
@@ -63,3 +56,4 @@ function displayCoord(coordinates) {
     }).addTo(map); 
 }
 
+loadCoord();
